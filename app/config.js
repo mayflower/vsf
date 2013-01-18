edd1b12d747a0c138d5f06cc097640407ed944c3
Christian Albrecht <christian.albrecht@mayflower.de>
2013-01-18 15:15:11 +0100

    Require ajax and event for zepto

--- app/config.js
+++ app/config.js
@@ -3,21 +3,36 @@ require.config({
 
     paths: {
         zepto: "../lib/zepto/src/zepto",
+        ajax: "../lib/zepto/src/ajax",
+        event: "../lib/zepto/src/event",
         lodash: "../lib/lodash/lodash",
         backbone: "../lib/backbone/backbone",
     },
 
     shim: {
         backbone: {
-            deps: ["lodash", "zepto"],
+            deps: ["lodash", "zepto", "event", "ajax"],
             exports: "Backbone"
         },
         zepto: {
-            exports: "$"
+            exports: "Zepto"
+        },
+        ajax: {
+            deps: ['zepto']
+        },
+        event: {
+            deps: ['zepto']
         }
     }
 });
 
 require(['backbone'], function(ding) {
-    console.log(ding);
+    require(['images'], function(Images) {
+        List = new Images();
+        List.fetch({
+            success: function()  {
+                console.dir(List.pluck('url'));
+            }
+        });
+    });
 });
